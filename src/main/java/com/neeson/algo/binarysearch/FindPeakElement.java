5afcea62cab95be86a796b45758c367f7564c3f8
neeson <1043358774@qq.com>
2020-04-10 18:15:50 +0800

    二分查找

--- src/main/java/com/neeson/algo/binarysearch/FindPeakElement.java
+++ src/main/java/com/neeson/algo/binarysearch/FindPeakElement.java
@@ -1,7 +1,5 @@
 package com.neeson.algo.binarysearch;
 
-import java.util.Arrays;
-
 /**
  * Create on 2020-04-09
  *
@@ -16,6 +14,19 @@ public class FindPeakElement {
             }
         }
         return nums.length - 1;
+    }
+
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 
 }
